56d4f1e1a46f500bcd8157db34006279aadf561f
Anamul-Deoun <anamulby2005@gmail.com>
2026-03-08 00:17:16 +0600

    added login page functionality

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,13 @@
-console.log('script file added!')
+document.getElementById('login-btn').addEventListener('click', () => {
+    const userName = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (userName === 'admin' && password === 'admin123') {
+        alert('Login Successfully....!')
+        window.location.href = "main.html";
+    }
+    else{
+        alert("Wrong username or password");
+    }
+
+})
